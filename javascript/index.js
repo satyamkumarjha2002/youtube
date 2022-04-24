a397361fe29fe5a9b7ee805271efbca2426f4c18
Satyam Kumar Jha <87626810+satyamkumarjha2002@users.noreply.github.com>
2022-04-24 22:18:50 +0530

    youtbe final

---
+++ javascript/index.js
@@ -0,0 +1,105 @@
+
+function fetch_data() {
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=100&q=coding+comedy+game&key=AIzaSyCXFk0Vqc56TlbVEdYqE7P7kSleurLkiAU`)
+        .then(function (res) {
+            return res.json();
+        }).then(function (res) {
+            //console.log(res.items);
+            display_trending(res.items)
+        })
+
+}
+let main_data=0;
+function fetch_search_data() {
+    let query = document.querySelector("#query").value;
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${query}&key=AIzaSyCXFk0Vqc56TlbVEdYqE7P7kSleurLkiAU`)
+        .then(function (res) {
+            return res.json();
+        }).then(function (res) {
+            main_data=res.items;
+            search_auto(main_data);
+        });
+}
+fetch_data();
+function display_trending(trending_data) {
+    localStorage.setItem("play_data",JSON.stringify(trending_data));
+    let main = document.querySelector("#main-content");
+    main.innerHTML = null;
+    trending_data.forEach(function(element,index) {
+        let div = document.createElement("div");
+        div.addEventListener("click",function(){
+            set_index(index);
+        })
+        let snippet = element.snippet;
+        let thumbnails = snippet.thumbnails.high;
+        let div1 = document.createElement("div");
+        let img = document.createElement("img");
+        img.src = thumbnails.url;
+        div1.append(img);
+        let div2 = document.createElement("div");
+        div2.setAttribute("id", "details");
+        let chanel_name = document.createElement("h5");
+        chanel_name.innerText = snippet.channelTitle;
+        let title = document.createElement("p");
+        title.innerText = snippet.title;
+        div2.append(title, chanel_name)
+        div.append(div1, div2);
+        main.append(div);
+        div.addEventListener("click", function () {
+            play_video(element);
+        })
+    });
+}
+
+function play_video(ele) {
+    //console.log(ele);
+}
+
+function search_auto(data) {
+    let search_res = document.querySelector("#search_res");
+    let user_input = document.querySelector("#query").value;
+    let main_content = document.querySelector("#main-content");
+    console.log(user_input);
+    search_res.style.display = "block"
+    search_res.innerHTML = null;
+    main_content.style.zIndex = -1;
+    if (user_input == "") {
+        main_content.style.zIndex = 1;
+        search_res.style.display = "none";
+    } else {
+        data.map(function (ele) {
+            let div = document.createElement("div");
+            div.addEventListener("click", function () {
+                search_data(data);
+            })
+            let snippet = ele.snippet;
+            let title = snippet.title;
+            let p = document.createElement("p");
+            p.innerText = title;
+            div.append(p);
+            search_res.append(div);
+        })
+    }
+
+}
+
+function search_data(data) {
+    localStorage.setItem("play_data",JSON.stringify(data));
+    window.location.href="search_result.html"
+}
+let id;
+
+function main() {
+        fetch_search_data();
+}
+
+function debaunce(func,delay) {
+    clearTimeout(id);
+    id=setTimeout(() => {
+        func();
+    }, delay);
+}
+function set_index(index){
+    localStorage.setItem("index",JSON.stringify(index));
+    window.location.href="show_video.html";
+}
---
+++ javascript/navbar.js
@@ -0,0 +1,22 @@
+let i = 0;
+
+function display_menu() {
+    if (i % 2 == 0) {
+        let menu_details = document.querySelector("#controls-details");
+        let menu_icons = document.querySelector("#controls-icons");
+        menu_icons.style.display = "none"
+        menu_details.style.display = "block"
+        document.querySelector("#main-content").style.width = "85%"
+    } else {
+        let menu_details = document.querySelector("#controls-details");
+        let menu_icons = document.querySelector("#controls-icons");
+        menu_icons.style.display = "block"
+        menu_details.style.display = "none"
+        document.querySelector("#main-content").style.width = "95%"
+    }
+    ++i;
+}
+
+function home_page() {
+    window.location.href = "index.html"
+}
